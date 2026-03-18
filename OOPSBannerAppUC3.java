6268e2423ede70e7c214206acb4ae988910de5ce
venkateswaralukallagunta-web <venkateswaralukallagunta@gmail.com>
2026-03-18 10:07:02 +0530

    Add files via upload

---
+++ OOPSBannerAppUC3.java
@@ -0,0 +1,26 @@
+
+public class OOPSBannerAppUC3 {
+
+    public static void main(String[] args) {
+        System.out.println(String.join(" ",
+                "  ***  ", "  ***  ", " ***** ", " ***** "));
+
+        System.out.println(String.join(" ",
+                " *   * ", " *   * ", " *   * ", " *     "));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", " *   * ", " *     "));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", " ***** ", " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", " *     ", "     * "));
+
+        System.out.println(String.join(" ",
+                " *   * ", " *   * ", " *     ", "     * "));
+
+        System.out.println(String.join(" ",
+                "  ***  ", "  ***  ", " *     ", " ***** "));
+    }
+}
